fa42a41f8ce6b5c1bf03b1f1cd97d27aebbcdd87
Yash Gupta <90241440+yash8917@users.noreply.github.com>
2021-11-13 18:14:13 +0530

    Update Boble Short in C.c

--- Boble Short in C.c
+++ Boble Short in C.c
@@ -1,12 +1,9 @@
 #include<stdio.h>
 
 int main(){
-
    int count, temp, i, j, number[30];
-
    printf("How many numbers are u going to enter?: ");
    scanf("%d",&count);
-
    printf("Enter %d numbers: ",count);
 
    for(i=0;i<count;i++)
